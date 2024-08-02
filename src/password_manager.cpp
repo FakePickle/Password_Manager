c528443752e6f69ea5b740bb4516fb94161cbff4
fakepickle <harshmistry159@gmail.com>
2024-08-02 19:57:25 +0530

    Added comments and code readablity

--- src/password_manager.cpp
+++ src/password_manager.cpp
@@ -1,44 +1,54 @@
+// Description: Implementation of the PasswordManager class.
+
 #include <password_manager/password_manager.hpp>
 #include <password_manager/encryption.hpp>
 #include <password_manager/database.hpp>
 
-PasswordManager::PasswordManager(const string& db_name, const string& master_password)
-    : db(db_name), masterPasswordHash(hashMasterPassword(master_password)) {
+PasswordManager::PasswordManager(const string &db_name, const string &master_password)
+    : db(db_name), masterPasswordHash(hashMasterPassword(master_password))
+{
     db.initializeDatabase();
 }
 
 PasswordManager::~PasswordManager() {}
 
-void PasswordManager::addPassword(const string& key, const string& password) {
+void PasswordManager::addPassword(const string &key, const string &password)
+{
     string encryptedPassword = encrypt(password, masterPasswordHash);
     string query = "INSERT INTO passwords (key, password) VALUES ('" + key + "', '" + encryptedPassword + "');";
     db.executeQuery(query);
 }
 
-void PasswordManager::removePassword(const string& key) {
+void PasswordManager::removePassword(const string &key)
+{
     string query = "DELETE FROM passwords WHERE key='" + key + "';";
     db.executeQuery(query);
 }
 
-string PasswordManager::getPassword(const string& key) {
+string PasswordManager::getPassword(const string &key)
+{
     string encryptedPassword = db.getPassword(key);
     return decrypt(encryptedPassword, masterPasswordHash);
 }
 
-void PasswordManager::printPasswords() {
+void PasswordManager::printPasswords()
+{
     db.printPasswords();
 }
 
-void PasswordManager::generatePassword(size_t length, const string& key) {
+void PasswordManager::generatePassword(size_t length, const string &key)
+{
     string lowCase = "abcdefghijklmnopqrstuvwxyz";
     string upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     string numbers = "0123456789";
     string special = "!@#$%^&*()_+-=[]{}|;:,.<>?";
 
     string password = "";
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < length; i++)
+    {
         int choice = rand() % 4;
-        switch (choice) {
+        switch (choice)
+        {
         case 0:
             password += lowCase[rand() % lowCase.length()];
             break;
@@ -58,20 +68,24 @@ void PasswordManager::generatePassword(size_t length, const string& key) {
     cout << "Do you want to save it? (y/n): ";
     char choice;
     cin >> choice;
-    if (choice == 'y') {
+    if (choice == 'y')
+    {
         addPassword(key, password);
     }
-    else {
+    else
+    {
         cout << "Password not saved." << endl;
     }
 }
 
-string PasswordManager::hashMasterPassword(const string& master_password) {
+string PasswordManager::hashMasterPassword(const string &master_password)
+{
     unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256(reinterpret_cast<const unsigned char*>(master_password.c_str()), master_password.length(), hash);
-    
+    SHA256(reinterpret_cast<const unsigned char *>(master_password.c_str()), master_password.length(), hash);
+
     stringstream ss;
-    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    {
         ss << hex << setw(2) << setfill('0') << static_cast<unsigned int>(hash[i]);
     }
     return ss.str();
